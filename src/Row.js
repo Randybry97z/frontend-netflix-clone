7941b688babdaf1b9ce7072bfd4603d22923639a
Bryan <bssandovalrod@gmail.com>
2020-09-19 09:13:20 -0500

    setting movies

--- src/Row.js
+++ src/Row.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from './axios'
 
-function Row({ title }) {
+function Row({ title, fetchUrl }) {
+  const [movies, setMovies] = useState([]);
+
+  //A snippet of code which runs based on a specific condition/variable
+  useEffect(() => {
+    // if [], run once when the row loads, and don't run again
+    async function fetchData() {
+      const request = await axios.get(fetchUrl);
+      setMovies(request.data.results)
+      return request;
+    }
+    fetchData();
+  }, [fetchUrl]);
+
   return (
     <div>
       <h2>{title}</h2>
 
       {/* container -> posters */}
-
     </div>
   )
 }
